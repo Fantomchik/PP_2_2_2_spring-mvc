bd0f3662074c505bfade0bd09941db29f83dd738
Fantomchik <Lordfantom455@gmail.com>
2025-03-05 05:26:02 +0300

    init

---
+++ PP_2_2_2_spring-mvc/src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,38 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CarServiceImpl implements CarService{
+
+
+    private List<Car> cars;
+
+    public CarServiceImpl() {
+        cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "Supra", 1500));
+        cars.add(new Car("Subaru", "Impreza", 300));
+        cars.add(new Car("Nissan", "Skyline", 500));
+        cars.add(new Car("Nissan", "370z", 400));
+        cars.add(new Car("Porsche", "911", 420));
+    }
+
+    public List<Car> outWithParam(Integer count){
+
+        if (count == null || count >= 5) {
+            return cars;
+        } else if (count == 0) {
+            throw new IllegalArgumentException("Count cannot be negative.");
+        } else {
+            return cars.stream()
+                    .limit(count)
+                    .collect(Collectors.toList());
+        }
+    }
+}
